0f51ad822219107fc80099056bbc82d1d7957953
Niek Versteege <niekversteege@gmail.com>
2013-03-29 23:11:11 +0100

    Update AirSocketMain.java
    Another jenkins test

--- src/main/java/com/dubbelepuntf/airsocketserver/AirSocketMain.java
+++ src/main/java/com/dubbelepuntf/airsocketserver/AirSocketMain.java
@@ -4,9 +4,7 @@ import com.dubbelepuntf.airsocketserver.cli.Server;
 
 public class AirSocketMain {
 
-	/**
-	 * Woop woop.
-	 * 
+	/** 
 	 * @param args
 	 */
 	public static void main(String[] args) {
