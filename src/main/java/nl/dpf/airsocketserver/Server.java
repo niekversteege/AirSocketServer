9d354eaade62ed1bb7bef102ff9c2b2220270980
niekversteege <niekversteege@gmail.com>
2013-05-27 15:38:33 +0200

    Refactor some crap. Test IntelliJ code style formatting.

--- src/main/java/nl/dpf/airsocketserver/Server.java
+++ src/main/java/nl/dpf/airsocketserver/Server.java
@@ -3,27 +3,32 @@ package nl.dpf.airsocketserver;
 import lombok.extern.log4j.Log4j;
 
 /**
- *  The server
+ * The server
  */
 @Log4j
-public class Server implements GameServer {
-    
-    public Server () {
-    	
+public class Server implements GameServer
+{
+
+    public Server()
+    {
+
     }
-    
-    public synchronized void run() {
-        
-       log.info("Booting up...");
-        
-        try {
-            while (!Thread.currentThread().isInterrupted()) {
-                
+
+    public synchronized void run()
+    {
+
+        log.info("Booting up...");
+
+        try
+        {
+            while(!Thread.currentThread().isInterrupted())
+            {
                 log.info("Heartbeat!");
                 wait(30000);
-                
             }
-        } catch (InterruptedException ex) {
+        }
+        catch(InterruptedException ex)
+        {
             log.error("interrupted: " + ex.getMessage(), ex);
         }
     }
--- src/main/java/nl/dpf/airsocketserver/cli/Command.java
+++ src/main/java/nl/dpf/airsocketserver/cli/Command.java
@@ -1,31 +1,30 @@
 package nl.dpf.airsocketserver.cli;
 
 /**
- *  
+ *
  */
-public enum Command {
-    
-    HELP("help", 0), 
-    OPEN("open", 1),   
+public enum Command
+{
+    HELP("help", 0),
+    OPEN("open", 1),
     TIME("time", 0);
-    
+
     private String name;
     private int numberOfArgs;
-    
-    Command(final String name, final int numberOfArgs) {
-        
+
+    Command(final String name, final int numberOfArgs)
+    {
         this.name = name;
         this.numberOfArgs = numberOfArgs;
-        
     }
-    
-    public String getName() {
-        
+
+    public String getName()
+    {
         return name;
     }
-    
-    public int getNumberOfArgs() {
-        
-        return numberOfArgs;       
+
+    public int getNumberOfArgs()
+    {
+        return numberOfArgs;
     }
 }
