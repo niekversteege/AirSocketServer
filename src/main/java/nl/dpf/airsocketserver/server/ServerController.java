bea33ecea06cdc7d00d45681e2db08130e61622f
Niek <niekversteege@gmail.com>
2013-06-15 02:11:35 +0200

    Add stopping mechanics. Still throws an exception on socket close. Needs to use ServerSocketChannel in future.

--- src/main/java/nl/dpf/airsocketserver/server/ServerController.java
+++ src/main/java/nl/dpf/airsocketserver/server/ServerController.java
@@ -3,6 +3,7 @@ package nl.dpf.airsocketserver.server;
 import lombok.extern.log4j.Log4j;
 import nl.dpf.airsocketserver.connections.ConnectionHandler;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
@@ -39,16 +40,21 @@ public class ServerController implements Observer {
 
     public synchronized boolean stop()
     {
-        connectionHandler.stop();
+        boolean success = false;
+        try {
+            connectionHandler.getServerSocket().close();
+            success = true;
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
 
-        return true;
+        return success;
     }
 
     @Override
     public void update(Observable o, Object arg) {
         /* TODO: receive updates from ConnectionHandler and pass it to the ClientFactory */
         /* TODO: add new client to list */
-        log.debug("update called.");
-        log.debug("Connected clients: " + (++nrOfClients));
+        log.debug("Connected clients: " + ++nrOfClients);
     }
 }
